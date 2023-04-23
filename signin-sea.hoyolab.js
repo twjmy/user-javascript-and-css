a31f170b42004a2cb3cd061514a8487d5727e629
VJ <twjmy@msn.com>
2023-04-24 07:36:12 +0800

    Update signin-sea.hoyolab.js

--- signin-sea.hoyolab.js
+++ signin-sea.hoyolab.js
@@ -1,12 +1,13 @@
-~async function(
-activeDay=`.components-home-assets-__sign-content_---actived-day---2GukeS`,
-icon=`.mhy-hoyolab-account-block__avatar-icon`,
-more=`.components-home-assets-__sign-content_---more-icon---3LDx1D`
-){
-  while(
-  !document.querySelector(more)||
-  !document.querySelector(activeDay)||
-  document.querySelector(icon).src.indexOf(`data:image`)+1)
-    await new Promise(resolve=>requestAnimationFrame(resolve));
+~async function (
+  blah = `.components-home-assets-__sign-content_---`,
+  icon = `.mhy-hoyolab-account-block__avatar-icon`,
+  activeDay = blah + `actived-day---2GukeS`,
+  more = blah + `more-icon---3LDx1D`
+) {
+  while (
+    !document.querySelector(more) ||
+    !document.querySelector(activeDay) ||
+    document.querySelector(icon).src.indexOf(`data:image`) + 1)
+    await new Promise(resolve => requestAnimationFrame(resolve));
   document.querySelector(activeDay).click();
 }();
