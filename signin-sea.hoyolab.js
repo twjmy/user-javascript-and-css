06f8cf386961f8e768fb754cfb6640004d2baf3f
VJ <twjmy@msn.com>
2023-04-26 05:13:22 +0800

    📲include mobile

--- signin-sea.hoyolab.js
+++ signin-sea.hoyolab.js
@@ -1,13 +1,17 @@
 ~async function (
-  blah = `.components-home-assets-__sign-content_---`,
   icon = `.mhy-hoyolab-account-block__avatar-icon`,
-  // activeDay = blah+`actived-day---2GukeS`,
-  signWrapper = blah + `sign-wrapper---38rWqB`
+  blah = `.components-home-assets-__sign-content_---`,
+  blahM = `.components-m-assets-__index_---`,
+  uidM = blah + `nick-name---2I1pf3`,
+  signWrapper = blah + `sign-wrapper---38rWqB`,
+  signWrapperM = blahM + `sign-wrapper---3WcYRI`,
 ) {
   while (
-    1 + document.querySelector(icon).src.indexOf(`data:image`) ||
-    // !document.querySelector(activeDay)||
-    !document.querySelector(signWrapper))
-    await new Promise(resolve => requestAnimationFrame(resolve))
-  document.querySelector(activeDay).click();
+    !document.querySelector(signWrapper) &&
+    !document.querySelector(signWrapperM) ||
+    1 + document.querySelector(icon).src.indexOf(`data:image`) &&
+    !document.querySelector(uidM)
+  ) await new Promise(resolve => requestAnimationFrame(resolve))
+  document.querySelector(signWrapper).click();
+  document.querySelector(signWrapperM).click();
 }();
