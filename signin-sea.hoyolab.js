e1c5d86d65023896598b76c270d4736135f72c99
VJ <twjmy@msn.com>
2023-05-03 23:22:38 +0800

    🔡remove mobile support but *resign* supported

--- signin-sea.hoyolab.js
+++ signin-sea.hoyolab.js
@@ -1,17 +1,40 @@
 ~async function (
   icon = `.mhy-hoyolab-account-block__avatar-icon`,
   blah = `.components-home-assets-__sign-content_---`,
-  blahM = `.components-m-assets-__index_---`,
-  uidM = blah + `nick-name---2I1pf3`,
+  blahc = `.components-common-`,
   signWrapper = blah + `sign-wrapper---38rWqB`,
-  signWrapperM = blahM + `sign-wrapper---3WcYRI`,
+  mend = blah + `mend-btn---FhiXMP`,
+  remind = blahc + `remind-__index_---remind-btn---1IIL1i`,
+  finish = blahc + `task-__index_---task-finish---2y0qCh`,
+  resign = blahc + `resign-modal-__index_---resign-btn---3oGNt6`,
 ) {
   while (
-    !document.querySelector(signWrapper) &&
-    !document.querySelector(signWrapperM) ||
-    1 + document.querySelector(icon).src.indexOf(`data:image`) &&
-    !document.querySelector(uidM)
+    !document.querySelector(signWrapper) ||
+    document.querySelector(icon).src.indexOf(`data:image`) == 0
   ) await new Promise(resolve => requestAnimationFrame(resolve))
   document.querySelector(signWrapper).click();
-  document.querySelector(signWrapperM).click();
+  while (
+    document.querySelector(`.miss-num`).innerHTML == 0
+  ) await new Promise(resolve => requestAnimationFrame(resolve))
+  ~function resignC() {
+    if (document.querySelector(mend))
+      document.querySelector(mend).click();
+    if (document.querySelector(resign))
+      document.querySelector(resign).click();
+  }();
+  if (document.querySelector(remind))
+    document.querySelector(remind).click();
+  while (
+    !document.querySelector(`.task-btn`)
+  ) await new Promise(resolve => requestAnimationFrame(resolve))
+  ~async function taskBtnClick(query) {
+    while (
+      document.querySelector(query) &&
+      document.querySelectorAll(finish).length < 3
+    ) for (const taskBtn of document.querySelectorAll(query)) {
+      await new Promise(resolve => requestAnimationFrame(resolve))
+      taskBtn.click();
+    }
+  }(`.task-btn`);
+  resignC();
 }();
